1a82a6c91b60a7d6cb514c0d337f1dc8144e4dc4
looks <lu.oliveira.silva98@gmail.com>
2023-08-13 16:59:04 +0100

    Project renamed

--- src/main/java/br/personal/GamingPocketStatus/GamingPocketStatusApplication.java
+++ src/main/java/br/personal/GamingPocketStatus/GamingPocketStatusApplication.java
@@ -1,13 +1,13 @@
-package br.personal.GammingPocketStatus;
+package br.personal.GamingPocketStatus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GammingPocketStatusApplication {
+public class GamingPocketStatusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GammingPocketStatusApplication.class, args);
+		SpringApplication.run(GamingPocketStatusApplication.class, args);
 	}
 
 }
--- src/test/java/br/personal/GamingPocketStatus/GamingPocketStatusApplicationTests.java
+++ src/test/java/br/personal/GamingPocketStatus/GamingPocketStatusApplicationTests.java
@@ -1,10 +1,10 @@
-package br.personal.GammingPocketStatus;
+package br.personal.GamingPocketStatus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GammingPocketStatusApplicationTests {
+class GamingPocketStatusApplicationTests {
 
 	@Test
 	void contextLoads() {
