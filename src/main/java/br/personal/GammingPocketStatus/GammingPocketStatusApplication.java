6d0c5bc12a8152bf2a318c9c406582dd94eb9c12
looks <lu.oliveira.silva98@gmail.com>
2023-08-13 16:38:21 +0100

    Main - Project structure

---
+++ src/main/java/br/personal/GammingPocketStatus/GammingPocketStatusApplication.java
@@ -0,0 +1,13 @@
+package br.personal.GammingPocketStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GammingPocketStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GammingPocketStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/personal/GammingPocketStatus/GammingPocketStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.personal.GammingPocketStatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GammingPocketStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
